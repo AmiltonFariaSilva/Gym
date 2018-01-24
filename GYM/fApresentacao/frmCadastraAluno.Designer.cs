f0e3530e3f3022bda3acf820a3bc5a073eb38faa
Amilton Faria Silva <35498695+AmiltonFariaSilva@users.noreply.github.com>
2018-01-24 08:40:54 -0200

    hello World
    Primeira versão funcionando

---
+++ GYM/fApresentacao/frmCadastraAluno.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace fApresentacao
+{
+    partial class frmCadastrarAluno
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMatricula = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblDtNascimento = new System.Windows.Forms.Label();
+            this.lblSexo = new System.Windows.Forms.Label();
+            this.lblCPF = new System.Windows.Forms.Label();
+            this.lblRG = new System.Windows.Forms.Label();
+            this.txbIdCliente = new System.Windows.Forms.TextBox();
+            this.txbNome = new System.Windows.Forms.TextBox();
+            this.dtpData = new System.Windows.Forms.DateTimePicker();
+            this.rbMasculino = new System.Windows.Forms.RadioButton();
+            this.rbFeminio = new System.Windows.Forms.RadioButton();
+            this.txbCPF = new System.Windows.Forms.TextBox();
+            this.lblPagamento = new System.Windows.Forms.Label();
+            this.cbOk = new System.Windows.Forms.CheckBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.txbRG = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMatricula
+            // 
+            this.lblMatricula.AutoSize = true;
+            this.lblMatricula.Location = new System.Drawing.Point(10, 31);
+            this.lblMatricula.Name = "lblMatricula";
+            this.lblMatricula.Size = new System.Drawing.Size(50, 13);
+            this.lblMatricula.TabIndex = 10;
+            this.lblMatricula.Text = "Matricula";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(131, 31);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 12;
+            this.lblNome.Text = "Nome";
+            // 
+            // lblDtNascimento
+            // 
+            this.lblDtNascimento.AutoSize = true;
+            this.lblDtNascimento.Location = new System.Drawing.Point(10, 83);
+            this.lblDtNascimento.Name = "lblDtNascimento";
+            this.lblDtNascimento.Size = new System.Drawing.Size(104, 13);
+            this.lblDtNascimento.TabIndex = 13;
+            this.lblDtNascimento.Text = "Data de Nascimento";
+            // 
+            // lblSexo
+            // 
+            this.lblSexo.AutoSize = true;
+            this.lblSexo.Location = new System.Drawing.Point(240, 83);
+            this.lblSexo.Name = "lblSexo";
+            this.lblSexo.Size = new System.Drawing.Size(31, 13);
+            this.lblSexo.TabIndex = 16;
+            this.lblSexo.Text = "Sexo";
+            // 
+            // lblCPF
+            // 
+            this.lblCPF.AutoSize = true;
+            this.lblCPF.Location = new System.Drawing.Point(10, 135);
+            this.lblCPF.Name = "lblCPF";
+            this.lblCPF.Size = new System.Drawing.Size(27, 13);
+            this.lblCPF.TabIndex = 14;
+            this.lblCPF.Text = "CPF";
+            // 
+            // lblRG
+            // 
+            this.lblRG.AutoSize = true;
+            this.lblRG.Location = new System.Drawing.Point(240, 135);
+            this.lblRG.Name = "lblRG";
+            this.lblRG.Size = new System.Drawing.Size(23, 13);
+            this.lblRG.TabIndex = 15;
+            this.lblRG.Text = "RG";
+            // 
+            // txbIdCliente
+            // 
+            this.txbIdCliente.Location = new System.Drawing.Point(13, 47);
+            this.txbIdCliente.Name = "txbIdCliente";
+            this.txbIdCliente.ReadOnly = true;
+            this.txbIdCliente.Size = new System.Drawing.Size(100, 20);
+            this.txbIdCliente.TabIndex = 11;
+            // 
+            // txbNome
+            // 
+            this.txbNome.Location = new System.Drawing.Point(134, 47);
+            this.txbNome.Name = "txbNome";
+            this.txbNome.Size = new System.Drawing.Size(309, 20);
+            this.txbNome.TabIndex = 0;
+            // 
+            // dtpData
+            // 
+            this.dtpData.Location = new System.Drawing.Point(13, 99);
+            this.dtpData.Name = "dtpData";
+            this.dtpData.Size = new System.Drawing.Size(216, 20);
+            this.dtpData.TabIndex = 1;
+            // 
+            // rbMasculino
+            // 
+            this.rbMasculino.AutoSize = true;
+            this.rbMasculino.Location = new System.Drawing.Point(242, 101);
+            this.rbMasculino.Name = "rbMasculino";
+            this.rbMasculino.Size = new System.Drawing.Size(73, 17);
+            this.rbMasculino.TabIndex = 2;
+            this.rbMasculino.TabStop = true;
+            this.rbMasculino.Text = "Masculino";
+            this.rbMasculino.UseVisualStyleBackColor = true;
+            // 
+            // rbFeminio
+            // 
+            this.rbFeminio.AutoSize = true;
+            this.rbFeminio.Location = new System.Drawing.Point(321, 101);
+            this.rbFeminio.Name = "rbFeminio";
+            this.rbFeminio.Size = new System.Drawing.Size(67, 17);
+            this.rbFeminio.TabIndex = 3;
+            this.rbFeminio.TabStop = true;
+            this.rbFeminio.Text = "Feminino";
+            this.rbFeminio.UseVisualStyleBackColor = true;
+            // 
+            // txbCPF
+            // 
+            this.txbCPF.Location = new System.Drawing.Point(13, 151);
+            this.txbCPF.Name = "txbCPF";
+            this.txbCPF.Size = new System.Drawing.Size(200, 20);
+            this.txbCPF.TabIndex = 5;
+            // 
+            // lblPagamento
+            // 
+            this.lblPagamento.AutoSize = true;
+            this.lblPagamento.Location = new System.Drawing.Point(415, 83);
+            this.lblPagamento.Name = "lblPagamento";
+            this.lblPagamento.Size = new System.Drawing.Size(28, 13);
+            this.lblPagamento.TabIndex = 17;
+            this.lblPagamento.Text = "OK?";
+            // 
+            // cbOk
+            // 
+            this.cbOk.AutoSize = true;
+            this.cbOk.Location = new System.Drawing.Point(418, 104);
+            this.cbOk.Name = "cbOk";
+            this.cbOk.Size = new System.Drawing.Size(15, 14);
+            this.cbOk.TabIndex = 4;
+            this.cbOk.UseVisualStyleBackColor = true;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(463, 480);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 7;
+            this.btnSalvar.Text = "&Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(554, 480);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 8;
+            this.btnCancelar.Text = "&Cancelar";
+            this.btnCancelar.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // txbRG
+            // 
+            this.txbRG.Location = new System.Drawing.Point(243, 151);
+            this.txbRG.Name = "txbRG";
+            this.txbRG.Size = new System.Drawing.Size(200, 20);
+            this.txbRG.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Cadastrar Aluno";
+            // 
+            // frmCadastrarAluno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(636, 510);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txbRG);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.cbOk);
+            this.Controls.Add(this.lblPagamento);
+            this.Controls.Add(this.txbCPF);
+            this.Controls.Add(this.rbFeminio);
+            this.Controls.Add(this.rbMasculino);
+            this.Controls.Add(this.dtpData);
+            this.Controls.Add(this.txbNome);
+            this.Controls.Add(this.txbIdCliente);
+            this.Controls.Add(this.lblRG);
+            this.Controls.Add(this.lblCPF);
+            this.Controls.Add(this.lblSexo);
+            this.Controls.Add(this.lblDtNascimento);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.lblMatricula);
+            this.Controls.Add(this.btnCancelar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmCadastrarAluno";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastrar Aluno";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMatricula;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblDtNascimento;
+        private System.Windows.Forms.Label lblSexo;
+        private System.Windows.Forms.Label lblCPF;
+        private System.Windows.Forms.Label lblRG;
+        private System.Windows.Forms.TextBox txbIdCliente;
+        private System.Windows.Forms.TextBox txbNome;
+        private System.Windows.Forms.DateTimePicker dtpData;
+        private System.Windows.Forms.RadioButton rbMasculino;
+        private System.Windows.Forms.RadioButton rbFeminio;
+        private System.Windows.Forms.TextBox txbCPF;
+        private System.Windows.Forms.Label lblPagamento;
+        private System.Windows.Forms.CheckBox cbOk;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.TextBox txbRG;
+        private System.Windows.Forms.Label label1;
+    }
+}
